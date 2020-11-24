f8f734e2aa2be27fb2ea728c8a927b4dd0a36d12
cedricve <cedric@verstraeten.io>
2020-11-24 12:43:45 +0100

    Update index.py

--- examples/kafka-handler/step5-python/index.py
+++ examples/kafka-handler/step5-python/index.py
@@ -1,5 +1,8 @@
 from mqueue import CreateQueue
 import requests
+import cv2
+import numpy as np
+from sklearn.cluster import KMeans
 
 AccessKey = 'xxx'
 SecretAccessKey = 'xxx'
@@ -10,6 +13,25 @@ kafkaQueue = CreateQueue(queueName='kerberos-storage-example-queue',
                          security='SASL_PLAINTEXT',
                          username= 'aaa',
                          password='xxx')
+
+def calculateHistogram(img):
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img.reshape((img.shape[0] * img.shape[1],3)) #represent as row*column,channel number
+    clt = KMeans(n_clusters=3)
+    clt.fit(img)
+
+    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+    (hist, _) = np.histogram(clt.labels_, bins=numLabels)
+
+    hist = hist.astype("float")
+    hist /= hist.sum()
+
+    rgbs = []
+    for cluster in clt.cluster_centers_:
+        rgbs.append(list(map(int,cluster)))
+    return rgbs
+
 while True:
     try:
         messages = kafkaQueue.ReceiveMessages()
@@ -36,6 +58,16 @@ while True:
             fileContent = response.content
             with open('video.mp4', 'wb') as file:
                 file.write(fileContent)
+
+            vidcap = cv2.VideoCapture('video.mp4')
+            success, img = vidcap.read()
+
+            histogram = []
+            if success:
+                histogram = calculateHistogram(img)
+            else:
+                print("Something went wrong while capturing a frame from video.mp4")
+            print(histogram)
 
             print("next..")
 
